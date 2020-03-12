0941f94f0cb1c6db953e57bb3116685d7bd1d311
Danny Swift <dswift333@gmail.com>
2020-03-11 23:06:35 -0400

    running but not scoring well

--- Artificial Intelligence/PlayerAI.py
+++ Artificial Intelligence/PlayerAI.py
@@ -29,9 +29,14 @@ class Node:
         return self.get_min_children()
 
     def get_max_children(self):
+        children = []
         moves = self.grid.getAvailableMoves()
+        for move in moves:
+            new = self.grid.clone()
+            new.move(move)
+            children.append((new, move))
 
-        return [(self.grid.clone().move(move), move) for move in moves]
+        return children
 
     def get_min_children(self):
         children = []
@@ -48,7 +53,7 @@ class Node:
 
 
 class PlayerAI(BaseAI):
-    def __init__(self, depth=6):
+    def __init__(self, depth=4):
         self.depth = depth
 
     def getMove(self, grid):
@@ -64,13 +69,13 @@ class PlayerAI(BaseAI):
 
     def alphabeta(self, node, depth, alpha=-math.inf, beta=math.inf):
         if (depth == 0) or (not node.grid.canMove()):
-            return self.heuristic(node.grid)
+            return self.heuristic(node.grid), 0
         if node.turn == 1:
             val = -math.inf
             chosen_move = 0
             for child, move in node.get_children():
                 ival = val
-                val = max(val, self.alphabeta(Node(child, parent=node), depth - 1, alpha, beta))
+                val = max(val, self.alphabeta(Node(child, parent=node), depth - 1, alpha, beta)[0])
                 if ival != val:
                     chosen_move = move
                 alpha = max(alpha, val)
@@ -80,9 +85,8 @@ class PlayerAI(BaseAI):
         else:
             val = math.inf
             for child in node.get_children():
-                val = min(val, self.alphabeta(Node(child, parent=node), depth - 1, alpha, beta))
+                val = min(val, self.alphabeta(Node(child, parent=node), depth - 1, alpha, beta)[0])
                 beta = min(beta, val)
                 if alpha >= beta:
                     break
-            return val
-
+            return val, 0

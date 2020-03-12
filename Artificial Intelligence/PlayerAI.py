3be8615c6bc28ba02c9edf1b69e9f785c79faa42
Danny Swift <dswift333@gmail.com>
2020-03-12 10:29:46 -0400

    obsolescing Node class

--- Artificial Intelligence/PlayerAI.py
+++ Artificial Intelligence/PlayerAI.py
@@ -11,12 +11,8 @@ from BaseAI import BaseAI
 
 
 class Node:
-    def __init__(self, grid, parent=None):
+    def __init__(self, grid):
         self.grid = grid
-        if parent:
-            self.turn = -1 * parent.turn
-        else:
-            self.turn = 1
 
     def get_children(self):
         if self.turn == 1:
@@ -52,20 +48,48 @@ class PlayerAI(BaseAI):
         self.depth = depth
 
     def getMove(self, grid):
-        return self.alphabeta(Node(grid.clone()), self.depth)[1]
+        return self.alphabeta(grid.clone(), self.depth)[1]
+
+    def get_children(self, grid):
+        if self.turn == 1:
+            return self.get_max_children()
+        return self.get_min_children()
+
+    def get_max_children(self, grid):
+        children = []
+        moves = self.grid.getAvailableMoves()
+        for move in moves:
+            new = self.grid.clone()
+            new.move(move)
+            children.append((new, move))
+
+        return children
+
+    def get_min_children(self, grid):
+        children = []
+        cells = self.grid.getAvailableCells()
+        for c in cells:
+            new = self.grid.clone()
+            new.setCellValue(c, 2)
+            children.append(new)
+            new = self.grid.clone()
+            new.setCellValue(c, 4)
+            children.append(new)
+
+        return children
 
     def heuristic(self, grid):
         return len(grid.getAvailableCells())
 
-    def alphabeta(self, node, depth, alpha=-math.inf, beta=math.inf):
-        if (depth == 0) or (not node.grid.canMove()):
-            return self.heuristic(node.grid), 0
-        if node.turn == 1:
+    def alphabeta(self, grid, depth, alpha=-math.inf, beta=math.inf, max_turn=True):
+        if (depth == 0) or (not grid.canMove()):
+            return self.heuristic(grid), 0
+        if max_turn:
             val = -math.inf
             chosen_move = 0
-            for child, move in node.get_max_children():
+            for child, move in self.get_max_children(grid):
                 ival = val
-                val = max(val, self.alphabeta(Node(child, parent=node), depth - 1, alpha, beta)[0])
+                val = max(val, self.alphabeta(child, depth - 1, alpha, beta, False)[0])
                 if ival != val:
                     chosen_move = move
                 alpha = max(alpha, val)
@@ -74,8 +98,8 @@ class PlayerAI(BaseAI):
             return val, chosen_move
         else:
             val = math.inf
-            for child in node.get_min_children():
-                cval, move = self.alphabeta(Node(child, parent=node), depth - 1, alpha, beta)
+            for child in self.get_min_children(grid):
+                cval, move = self.alphabeta(child, depth - 1, alpha, beta, True)
                 val = min(val, cval)
                 beta = min(beta, val)
                 if alpha >= beta:

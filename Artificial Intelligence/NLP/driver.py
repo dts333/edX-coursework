ac1f38c994a46551bfb3ff6214987b850bec1f98
Danny Swift <dswift333@gmail.com>
2020-05-26 00:53:31 -0400

    tfidf

--- Artificial Intelligence/NLP/driver.py
+++ Artificial Intelligence/NLP/driver.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import SGDClassifier
 
 train_path = (
@@ -31,6 +33,8 @@ if __name__ == "__main__":
         stops = f.read().split()
     unigramizer = CountVectorizer(stop_words=stops)
     bigramizer = CountVectorizer(analyzer="word", ngram_range=(2, 2), stop_words=stops)
+    tfidf_unigramizer = TfidfVectorizer(stop_words=stops)
+    tfidf_bigramizer = TfidfVectorizer(stop_words=stops, ngram_range=(2, 2), norm="l1")
     test = pd.read_csv(test_path, encoding="ISO-8859-1")
     trainX = unigramizer.fit_transform(imdb.text)
     testX = unigramizer.transform(test.text)
@@ -45,9 +49,35 @@ if __name__ == "__main__":
         f.write(s)
     trainX = bigramizer.fit_transform(imdb.text)
     testX = bigramizer.transform(test.text)
+    clf = SGDClassifier(loss="hinge", penalty="l1")
     clf.fit(trainX, imdb["polarity"].astype("int"))
     preds = clf.predict(testX)
     s = ""
     for y in preds:
         s += str(y)
         s += os.linesep
+    with open("bigram.output.txt", "w") as f:
+        f.write(s)
+    trainX = tfidf_unigramizer.fit_transform(imdb.text)
+    testX = tfidf_unigramizer.transform(test.text)
+    clf = SGDClassifier(loss="hinge", penalty="l1")
+    clf.fit(trainX, imdb["polarity"].astype("int"))
+    preds = clf.predict(testX)
+    s = ""
+    for y in preds:
+        s += str(y)
+        s += os.linesep
+    with open("unigramtfidf.output.txt", "w") as f:
+        f.write(s)
+    trainX = tfidf_bigramizer.fit_transform(imdb.text)
+    testX = tfidf_bigramizer.transform(test.text)
+    clf = SGDClassifier(loss="hinge", penalty="l1")
+    clf.fit(trainX, imdb["polarity"].astype("int"))
+    preds = clf.predict(testX)
+    s = ""
+    for y in preds:
+        s += str(y)
+        s += os.linesep
+    with open("bigramtfidf.output.txt", "w") as f:
+        f.write(s)
+
